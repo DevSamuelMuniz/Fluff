c3394caef40df12a0c402dad4cfd4d45bd01d21f
DevSamuelMuniz <samuel@dev.com>
2023-12-12 00:29:40 -0300

    add inserção de dados no banco de dados com python

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
 from flask import Flask, render_template, request, jsonify
 import mysql.connector
-import json
 
 app = Flask(__name__)
 
+# Estabelece a conexão com o banco de dados
 db = mysql.connector.connect(
     host="localhost",
     user="root",
     password="12345",
-    database="usuarios" 
+    database="usuarios"
 )
 
-from flask import Flask, request, jsonify
+# Rota para a página inicial (index.html)
+@app.route('/')
+def index():
+    return render_template('index.html')  # Renderiza o arquivo index.html
 
-app = Flask(__name__)
+# Rota para a página de registro (registro.html)
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
 
-# Rota para receber os dados POST
-@app.route('/cadastro', methods=['POST'])
-def cadastra_usuario():
-    data = request.get_json()  # Obtém os dados enviados no corpo da solicitação
-    nome = data.get('name')
-    email = data.get('email')
-    senha = data.get('password')
 
-    # Faça o que desejar com esses dados, por exemplo, imprimi-los
-    print(f"Nome: {nome}, Email: {email}, Senha: {senha}")
+# Rota para receber os dados POST do formulário de registro
+@app.route('/cadastrar_usuario', methods=['POST'])
+def cadastrar_usuario():
+    if request.method == 'POST':
+        data = request.form  # Obtém os dados enviados pelo formulário
+        nome = data.get('nome')
+        email = data.get('email')
+        senha = data.get('senha')
 
-    # Aqui você pode retornar uma resposta se necessário
-    return jsonify({'message': 'Dados recebidos com sucesso!'})
+        # Insere os dados no banco de dados
+        cursor = db.cursor()
+        sql = "INSERT INTO usuario (nome, email, senha) VALUES (%s, %s, %s)"
+        val = (nome, email, senha)
+        cursor.execute(sql, val)
+        db.commit()
+
+        # Fecha o cursor e a conexão com o banco de dados
+        cursor.close()
+
+        # Redireciona para a página de registro após cadastrar o usuário
+        return render_template('registro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)  # Inicia o servidor Flask em modo de depuração
